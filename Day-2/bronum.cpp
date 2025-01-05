abf9146095535d4bcfc4cb7df07526c57a3c7cac
YajvendraSaini <yajvendra98@gmail.com>
2025-01-05 22:41:01 +0530

    if-else

---
+++ Day-2/bronum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int bronum;
+    cout << "Enter the number of bros:";
+
+    cin >> bronum;
+
+    if(bronum == 0){
+        cout << "baat ban jayegi" <<endl;
+    } else{
+        cout << "baat nhi banegi" <<endl;
+    }
+}
